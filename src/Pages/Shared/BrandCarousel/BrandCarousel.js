cdc9c54d479ce98b2266e4618b9ce75ee113dedb
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-11 02:05:29 +0600

    NavBar with responsive content toggle

--- src/Pages/Shared/BrandCarousel/BrandCarousel.js
+++ src/Pages/Shared/BrandCarousel/BrandCarousel.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import img1 from '../../../assets/brands/img1.jpg';
+import img2 from '../../../assets/brands/img2.png';
 
 
 const BrandCarousel = () => {
     return (
         <Carousel>
-      <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ExampleCarouselImage text="Third slide" />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+            <Carousel.Item>
+                <img className='d-block w-100' src={ img1 } alt='First Slide' />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className='d-block w-100' src={ img2 } alt='First Slide' />
+            </Carousel.Item>
     </Carousel>
     );
 };
