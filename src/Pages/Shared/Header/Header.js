d56f60315e5de11c51c3d2e4d8762ea4f1bcc2e8
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-20 23:10:45 +0600

    Load news category data from server

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,12 +4,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LeftSideNav from '../LeftSideNav/LeftSideNav';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary mb-4">
       <Container>
-        <Navbar.Brand href="#home">Dragon News</Navbar.Brand>
+        <Navbar.Brand><Link to='/'>Dragon News</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
