4f30246498250144e4bc012b697e5b5675f3cc8d
Shohan Sharoar <maffin.killer007@gmail.com>
2023-09-20 23:31:21 +0600

    Showing News Title and Image on the home page

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 const NewsSummaryCard = ({ news }) => {
+    const { _id, title, author, details, image_url } = news;
+    console.log(news);
     return (
-        <div>
-            { news.title }
-        </div>
+        <Card className="mb-5">
+            <Card.Header>Featured</Card.Header>
+                <Card.Body>
+                    <Card.Title>{ title }</Card.Title>
+                    <Card.Img variant='top' src={ image_url } />
+                        <Card.Text>
+                        {
+                            details.length > 200 ? 
+                            <p>{ details.slice(0, 250) + '...' }<Link to={ `/news/${_id}` }>Read More</Link></p>
+                            :
+                            <p>{ details }</p>
+                        }
+                        </Card.Text>
+                </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
     );
 };
 
