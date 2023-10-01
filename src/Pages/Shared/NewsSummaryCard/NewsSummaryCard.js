0733248222dc7432c4faf42e15bd607a98b727ff
Shohan Sharoar <maffin.killer007@gmail.com>
2023-10-01 23:13:02 +0600

    Author info added and fixed some design issues.

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 
 const NewsSummaryCard = ({ news }) => {
-    const { _id, title, author, details, image_url } = news;
+    const { _id, title, author, details, image_url, total_view } = news;
     console.log(news);
     return (
         <Card className="mb-5">
-            <Card.Header>Featured</Card.Header>
+            <Card.Header className='d-flex justify-content-between align-items-center'>
+                <div className='d-flex'>
+                    <Image className='me-2' src= { author.img } style={{ height: '60px' }} roundedCircle />
+                    <div>
+                        <p>{ author.name }</p>
+                        <p>{ author.published_date }</p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark />
+                    <FaShareAlt />
+                </div>
+            </Card.Header>
                 <Card.Body>
                     <Card.Title>{ title }</Card.Title>
                     <Card.Img variant='top' src={ image_url } />
