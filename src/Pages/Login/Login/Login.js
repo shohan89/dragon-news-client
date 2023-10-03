174d651af9e6ef00c0df2e72bf5c8e8ac37ffe24
Shohan Sharoar <maffin.killer007@gmail.com>
2023-10-03 21:03:24 +0600

    Recap and Display Login error and Private Route

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,37 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const { signIn } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleSignIn = event =>{
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
 
-        signIn(email, password)
-            .then(result =>{
-                const user = result.user;
-                console.log(user);
-                form.reset();
-                navigate('/');
-            })
-            .then(error => console.error(error));
-    }
+    signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setError("");
+        navigate("/");
+      })
+      .then((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+  };
   return (
-    <Form onSubmit={ handleSignIn }>
+    <Form onSubmit={handleSignIn}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control name="email" type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
+      <Form.Text className="text-danger">
+        { error }
+      </Form.Text>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,29 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const Register = () => {
-    const { createUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const { createUser } = useContext(AuthContext);
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const photoURL = form.photoURL.value;
-        const email = form.email.value;
-        const password = form.password.value;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoURL = form.photoURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
 
-        createUser(email, password)
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-                form.reset();
-            })
-            .catch(error => console.error(error));
-    }
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError("");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message);
+      });
+  };
   return (
-    <Form onSubmit={ handleSubmit }>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Your Name</Form.Label>
         <Form.Control name="name" type="text" placeholder="Your Name" />
@@ -57,7 +62,7 @@ const Register = () => {
       <Button variant="primary" type="submit">
         Register
       </Button>
-      <Form.Text className="text-muted"></Form.Text>
+      <Form.Text className="text-danger">{ error }</Form.Text>
     </Form>
   );
 };
